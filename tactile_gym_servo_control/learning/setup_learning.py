fbcb01cc49381df5cd8e43018a414060d13cd74c
nlepora <n.lepora@bristol.ac.uk>
2022-12-29 09:17:15 +0000

    collected data and trained models

--- tactile_gym_servo_control/learning/setup_learning.py
+++ tactile_gym_servo_control/learning/setup_learning.py
@@ -9,7 +9,7 @@ def setup_learning(save_dir):
     learning_params = {
         'seed': 42,
         'batch_size': 128,
-        'epochs': 50,#250
+        'epochs': 100,
         'lr': 1e-4,
         'lr_factor': 0.5,
         'lr_patience': 10,
