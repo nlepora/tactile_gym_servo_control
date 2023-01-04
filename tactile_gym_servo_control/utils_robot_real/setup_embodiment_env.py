b1baafbf318d3033292909699f63b6ff2953ac1d
nlepora <n.lepora@bristol.ac.uk>
2023-01-04 07:38:17 +0000

    uses cv2 only (no vsp)

--- tactile_gym_servo_control/utils_robot_real/setup_embodiment_env.py
+++ tactile_gym_servo_control/utils_robot_real/setup_embodiment_env.py
@@ -1,30 +1,39 @@
-import numpy as np
+import cv2
 
 from cri.robot import SyncRobot
-from cri.controller import Mg400Controller as Controller
-# from cri.controller import DummyController as Controller
-from vsp.video_stream import CvImageOutputFileSeq, CvVideoDisplay, CvPreprocVideoCamera   
-from vsp.processor import CameraStreamProcessor, AsyncProcessor
+# from cri.controller import Mg400Controller as Controller
+from cri.controller import DummyController as Controller
 
 
-def make_sensor(
-    size=[128, 128], 
-    crop=None, 
-    exposure=-7, 
-    source=0, 
-    threshold=None
-):  
-    camera = CvPreprocVideoCamera(
-        size, crop, threshold, exposure=exposure, source=source
-    )
-    
-    for _ in range(5): camera.read() # Hack - camera transient   
-    
-    return AsyncProcessor(CameraStreamProcessor(
-            camera=camera,
-            display=CvVideoDisplay(name='sensor'),
-            writer=CvImageOutputFileSeq())
-    )
+class Sensor:
+    def __init__(self,
+        size=[128, 128], 
+        crop=None, 
+        exposure=-7, 
+        source=0, 
+        threshold=None
+    ):  
+        self.size, self.crop, self.threshold = size, crop, threshold
+        self.cam = cv2.VideoCapture(source)
+        self.cam.set(cv2.CAP_PROP_EXPOSURE, exposure)
+        for _ in range(5): self.cam.read() # Hack - camera transient 
+    def process(self):
+        _, img = self.cam.read()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if self.crop is not None:
+            x0, y0, x1, y1 = self.crop
+            img = img[y0:y1, x0:x1]
+        if self.threshold is not None:
+            img = cv2.medianBlur(img, 5)
+            img = cv2.adaptiveThreshold(img, 255, 
+                cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 
+                *self.threshold
+            )
+        if self.size is not None:
+            img = cv2.resize(img, 
+                self.size, interpolation=cv2.INTER_AREA
+            )
+        return img
 
 
 def setup_embodiment_env(
@@ -47,13 +56,13 @@ def setup_embodiment_env(
     embodiment.coord_frame = workframe
 
     # setup the tactip
-    sensor = make_sensor(**tactip_params)
+    sensor = Sensor(**tactip_params)
 
     def sensor_process(outfile=None):
-        img = sensor.process(
-            num_frames=1, start_frame=1, outfile=outfile
-        )
-        return img[0,:,:,0]
+        img = sensor.process()
+        if outfile is not None:
+            cv2.imwrite(outfile, img)
+        return img
 
     embodiment.sensor_process = sensor_process
 
