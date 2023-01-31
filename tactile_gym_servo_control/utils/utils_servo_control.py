bbe6ddee53150d70f0332dae86487ebec04549bf
nlepora <n.lepora@bristol.ac.uk>
2023-01-31 06:21:25 +0000

    reimplemented slider_sim, alongside slide (tkinter)

--- tactile_gym_servo_control/utils/utils_servo_control.py
+++ tactile_gym_servo_control/utils/utils_servo_control.py
@@ -76,7 +76,8 @@ class Slider:
             self.pose_ids.append(
                 tk.Scale(self.tk, from_=pose_llims[i], to=pose_ulims[i],
                     label=label_name, length=400, orient=tk.HORIZONTAL, 
-                    tickinterval=(pose_ulims[i]-pose_llims[i])/4, resolution=0.1)
+                    tickinterval=(pose_ulims[i]-pose_llims[i])/4, resolution=0.1
+                )
             )
             self.pose_ids[i].pack()
             self.pose_ids[i].set(init_pose[i])
@@ -86,6 +87,29 @@ class Slider:
         self.tk.update()
         for i in range(len(pose)):
             pose[i] = self.pose_ids[i].get()
+        return pose
+
+
+class Slider_sim:
+    def __init__(self,
+        embodiment, init_pose,
+        pose_llims=[-5, -5, 0, -15, -15, -180],
+        pose_ulims=[ 5,  5, 5,  15,  15,  180]
+    ):    
+        self.embodiment = embodiment
+        self.pose_ids = []
+        for i, label_name in enumerate(POSE_LABEL_NAMES):
+            self.pose_ids.append(
+                embodiment.controller._client._sim_env._pb.addUserDebugParameter(
+                    label_name, pose_llims[i], pose_ulims[i], init_pose[i]
+                )
+            )
+
+    def read(self, pose):
+        for i in range(len(pose)):
+            pose[i] = self.embodiment.controller._client._sim_env._pb.readUserDebugParameter(
+                self.pose_ids[i]
+            ) 
         return pose
 
 
